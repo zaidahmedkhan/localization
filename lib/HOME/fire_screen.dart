d4f6d68f339386ec513cae744d60e934536806aa
John Doe <zaidkhan200415@gmail.com>
2024-05-10 17:42:58 +0500

    localization added!

---
+++ lib/HOME/fire_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FireView extends StatefulWidget {
+  const FireView({super.key});
+
+  @override
+  State<FireView> createState() => _FireView();
+}
+
+class _FireView extends State<FireView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            "Trendings screen".tr,
+            style: const TextStyle(color: Colors.black, fontSize: 30),
+          )),
+          Padding(
+            padding: const EdgeInsets.only(left: 300, top: 500),
+            child: FloatingActionButton(
+                onPressed: () {
+                  _showLanguageSelector(context);
+                },
+                child: const Icon(Icons.language_outlined)),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showLanguageSelector(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return ListView(
+          shrinkWrap: true,
+          children: [
+            _buildLanguageItem("English", const Locale('en', 'US')),
+            _buildLanguageItem("Urdu", const Locale('ur', 'PK')),
+            _buildLanguageItem("Hindi", const Locale('hi', 'IN')),
+            _buildLanguageItem("Chinese", const Locale('zh', 'CN')),
+            _buildLanguageItem("Arabic", const Locale('ar', 'AR')),
+            _buildLanguageItem("Bengali", const Locale('bn', 'BD')),
+            _buildLanguageItem("Spanish", const Locale('es', 'ES')),
+            _buildLanguageItem("Turkish", const Locale('tr', 'TR')),
+            _buildLanguageItem("Japanese", const Locale('ja', 'JP')),
+            _buildLanguageItem("Korean", const Locale('ko', 'KR')),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildLanguageItem(String language, Locale locale) {
+    return ListTile(
+      title: Text(language),
+      onTap: () {
+        Get.updateLocale(locale);
+        Navigator.pop(context);
+      },
+    );
+  }
+}
---
+++ lib/LANGUAGES/languages.dart
@@ -0,0 +1,87 @@
+// import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+// class Languages extends Translations {
+//   @override
+//   Map<String, Map<String, String>> get keys => {
+//         'en_US': {
+//           'Home screen': 'Home screen',
+//           'Trendings screen': 'Trendings screen',
+//           'Notification screen': 'Notification screen',
+//           'ProfileView screen': 'ProfileView screen',
+//         },
+//         'ur_PK': {
+//           'Home screen': 'ہوم اسکرین',
+//           'Trendings screen': 'موضوعی اسکرین',
+//           'Notification screen': 'اطلاعات کی اسکرین',
+//           'ProfileView screen': 'پروفائل اسکرین',
+//         }
+//       };
+// }
+
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+class Languages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {
+          'Home screen': 'Home screen',
+          'Trendings screen': 'Trendings screen',
+          'Notification screen': 'Notification screen',
+          'ProfileView screen': 'ProfileView screen',
+        },
+        'ur_PK': {
+          'Home screen': 'ہوم اسکرین',
+          'Trendings screen': 'موضوعی اسکرین',
+          'Notification screen': 'اطلاعات کی اسکرین',
+          'ProfileView screen': 'پروفائل اسکرین',
+        },
+        'hi_IN': {
+          'Home screen': 'होम स्क्रीन',
+          'Trendings screen': 'ट्रेंडिंग स्क्रीन',
+          'Notification screen': 'सूचना स्क्रीन',
+          'ProfileView screen': 'प्रोफ़ाइल दृश्य',
+        },
+        'zh_CN': {
+          'Home screen': '主屏幕',
+          'Trendings screen': '趋势屏幕',
+          'Notification screen': '通知屏幕',
+          'ProfileView screen': '个人资料视图',
+        },
+        'ar_AR': {
+          'Home screen': 'الشاشة الرئيسية',
+          'Trendings screen': 'الشاشة الرئيسية',
+          'Notification screen': 'شاشة الإشعارات',
+          'ProfileView screen': 'شاشة الملف الشخصي',
+        },
+        'bn_BD': {
+          'Home screen': 'হোম স্ক্রিন',
+          'Trendings screen': 'ট্রেন্ডিংস স্ক্রিন',
+          'Notification screen': 'বিজ্ঞপ্তি স্ক্রিন',
+          'ProfileView screen': 'প্রোফাইল ভিউ স্ক্রিন',
+        },
+        'es_ES': {
+          'Home screen': 'Pantalla de inicio',
+          'Trendings screen': 'Pantalla de tendencias',
+          'Notification screen': 'Pantalla de notificaciones',
+          'ProfileView screen': 'Pantalla de perfil',
+        },
+        'tr_TR': {
+          'Home screen': 'Ana ekran',
+          'Trendings screen': 'Trend ekranı',
+          'Notification screen': 'Bildirim ekranı',
+          'ProfileView screen': 'Profil ekranı',
+        },
+        'ja_JP': {
+          'Home screen': 'ホーム画面',
+          'Trendings screen': 'トレンド画面',
+          'Notification screen': '通知画面',
+          'ProfileView screen': 'プロフィール画面',
+        },
+        'ko_KR': {
+          'Home screen': '홈 화면',
+          'Trendings screen': '트렌드 화면',
+          'Notification screen': '알림 화면',
+          'ProfileView screen': '프로필 화면',
+        },
+      };
+}
